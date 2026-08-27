f296a3799227f0cd38fc663a39c9e36f0fe81e53
alessandro.macri <alessandro.macri95@gmail.com>
2026-08-27 12:56:10 +0200

    revert(navbar): "Vocazione" torna una voce come le altre
    "Lasciamo come prima, non fa niente" -- tre giri sul colore e
    sull'animazione (rame chiaro, poi rame, poi rame acceso col respiro)
    senza mai arrivare a un punto fermo. Non valeva la pena insistere su un
    dettaglio che, alla fine, la comunita' ha deciso di non volere.
    
    Ripristina esattamente lo stato di 034e5f3, l'ultimo commit prima che
    questo filone cominciasse: nessun'altra differenza.

--- src/components/layout/MdvNavbar.vue
+++ src/components/layout/MdvNavbar.vue
@@ -31,8 +31,7 @@
             <router-link
               v-else
               :class="['mdv-navlink', 'mdv-navlink--chiaro', 'barra__link',
-                       { 'mdv-navlink--attivo': voceAttiva($route.path, item.to),
-                         'barra__link--vocazione': item.to === '/vocazione' }]"
+                       { 'mdv-navlink--attivo': voceAttiva($route.path, item.to) }]"
               :to="item.to"
             >{{ item.title }}</router-link>
           </template>
@@ -103,12 +102,7 @@
                 <span class="fuori" aria-hidden="true">↗</span>
                 <span class="sr-only">(si apre in una nuova scheda)</span>
               </a>
-              <router-link
-                v-else
-                :class="['menu__link', { 'menu__link--vocazione': item.to === '/vocazione' }]"
-                :to="item.to"
-                @click="chiudi"
-              >
+              <router-link v-else class="menu__link" :to="item.to" @click="chiudi">
                 {{ item.title }}
               </router-link>
             </template>
